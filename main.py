c4a1b1c9f6c5c5d89c878fefdc68ad05bcc0b195
alebaro0375 <kimba2413@gmail.com>
2025-08-18 17:37:28 -0300

    fix: corregido import de seguridad_v2

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from verificacion_seguridad import verificar_entorno
-from estructura_subcarpetas import cargar_estructura, crear_subcarpetas_para_cuenta
-from archivado_automatico import procesar_archivos
-from generar_resumen_excel import actualizar_resumen
-from alertas_email import enviar_alerta
+from scripts.seguridad_v2 import (
+    verificar_entorno,
+    verificar_dependencias,
+    verificar_permisos,
+    verificar_hash
+)
+
+from scripts.verificacion_seguridad import verificar_entorno
+from scripts.estructura_subcarpetas import cargar_estructura, crear_subcarpetas_para_cuenta
+from scripts.archivado_automatico import procesar_archivos
+from scripts.generar_resumen_excel import actualizar_resumen
+from scripts.alertas_email import enviar_alerta
 
 def main():
     verificar_entorno()
